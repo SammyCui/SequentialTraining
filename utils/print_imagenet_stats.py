95c306fa6723ddb26d25d19b9a32a7fc3b6bef40
Sammy Cui <xcui16@stlawu.edu>
2022-09-29 12:56:02 -0400

    refactor

--- utils/print_imagenet_stats.py
+++ utils/print_imagenet_stats.py
@@ -1,12 +1,13 @@
 from collections import Counter
 
-from utils.data_utils import ResizeImageLoader, GenerateBackground
-from torch_datasets import ImagenetFolder
+from utils.data_utils import GenerateBackground
+from loader import ResizeImageLoader
+from datasets_legacy import ImagenetDataset
 
 test_loader = ResizeImageLoader((150, 150), p=0.2,
                                 background_generator=GenerateBackground(bg_type = 'color'),
                                 annotation_root_path='/u/erdos/cnslab/imagenet-bndbox/bndbox/')
-test_dataset = ImagenetFolder(cls_to_use=None, root='/u/erdos/cnslab/imagenet/',
+test_dataset = ImagenetDataset(cls_to_use=None, root='/u/erdos/cnslab/imagenet/',
                               loader=test_loader)
 print('n of classes: ', len(test_dataset.classes))
 train_dist = {k:dict(Counter(test_dataset.targets))[v] for k,v in test_dataset.class_to_idx.items()}
