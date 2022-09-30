22aedb68533fe06edbc886cf41f5e6d00bc93651
Xuanming Cui <xcui32@ciscluster.cm.cluster>
2022-09-30 00:38:51 -0400

    modify .sh file

--- smain.sh
+++ smain.sh
@@ -17,5 +17,4 @@ module load pytorch-py36-cuda10.1-gcc
 module load ml-pythondeps-py36-cuda10.1-gcc
 
 
-python3 /u/erdos/students/xcui32/SequentialTraining/main.py --regimens all --dataset_name VOC --train_size --test_size --num_classes --cls_to_use
---input_size '0.2,0.4,0.6,0.8,1'
+python3 /u/erdos/students/xcui32/SequentialTraining/main.py --regimens all --dataset_name VOC --train_size --test_size --num_classes --cls_to_use --input_size '0.2,0.4,0.6,0.8,1' --sizes 150 --resize_method long --lr 0.1 --epoch 200 --model resnet18 --lr_patience 3 --min_lr 0.00001 --n_folds 5 --n_folds_to_use 5 --save_progress_ckpt False --save_result_ckpt False --result_path ./results/VOC
